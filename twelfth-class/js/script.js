f408dfa706d6861ed88c122adf8ce6d8a06bf2fe
Kabit Khadka <kabitkhadka1@gmail.com>
2026-03-11 11:20:57 +0545

    Js started

---
+++ twelfth-class/js/script.js
@@ -0,0 +1,19 @@
+ console.log("hello")
+ var c=7;
+var b=9;
+var sum = b+c;
+console.log(sum);
+
+let x = 25;
+let y = 4;
+let remainder = x % y;
+console.log(remainder);
+
+
+let a = 5;
+if (true) {
+    console.log(a);
+    let a = 7;
+    console.log(a); 
+}
+console.log(a);
