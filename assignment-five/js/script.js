08b19bb7229bb0543410d17f286d5efd0e995596
Kabit Khadka <kabitkhadka1@gmail.com>
2026-03-29 09:42:24 +0545

    Assignment-five

---
+++ assignment-five/js/script.js
@@ -0,0 +1,12 @@
+let num = [1, 2, 3, 4, 5];
+let newNum = [];
+
+for(let i = 0; i < num.length; i++) {
+    if(num[i] % 2 === 0) {
+        newNum.push(num[i] + 3);  // Even numbers
+    } else {
+        newNum.push(num[i] + 7);  // Odd numbers
+    }
+}
+
+console.log(newNum);  // [8, 5, 10, 7, 12]
